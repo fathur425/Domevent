b8c12f6d1c9af5e12b68172bf3c3e70429880983
fathur425 <rahmanfa96@gmail.com>
2020-10-02 23:07:40 +0700

    update kedua

--- main.js
+++ main.js
@@ -12,3 +12,16 @@ const closeModal =()=>{
     let modal= document.getElementById(`modalMenu`)
     modal.style.display = "none" 
 }
+//kita ambil element yang diinginkan
+let showcase = document.getElementById(`wrapper_showcase`)
+//let nama = `halooooo`
+//console.log(showcase)
+//kita inject elementnya dengan synttax html
+//showcase.innerHTML += `
+    //<img src="https://picsum.photos/seed/56/300" alt="ini gambar no 13" />
+//`
+for(var i=1; i<40; i++){
+    showcase.innerHTML += `
+    <img src="https://picsum.photos/seed/${i}/100" alt="ini gambar ke ${i}"/>
+    `
+}
